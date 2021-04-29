22e5953d2a7f44b80baea040449cefb903441c7e
Pranjali-711 <83390776+Pranjali-711@users.noreply.github.com>
2021-04-29 14:18:04 +0530

    Create forLoop.java

---
+++ forLoop.java
@@ -0,0 +1,10 @@
+
+public class Loop {
+    public static void main(String[] args) {
+        int n =8;
+        for (int i =1 ;i<11;i++)
+        {
+            System.out.println(n+" X "+i+" = "+ n*i);
+        }
+    }
+}
